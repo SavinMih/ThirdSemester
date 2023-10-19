581f7c36ca9b8ee553fd661a324d89327bf6ee3f
MEMass <sawin.world@gmail.com>
2023-10-19 22:09:56 +0500

    Lab 4 completed

--- labs/src/main/java/com/sawin/labs/service/ModifyRequestService.java
+++
@@ -1,7 +0,0 @@
-package com.sawin.labs.service;
-
-import com.sawin.labs.model.Request;
-
-public interface ModifyRequestService {
-    void modifyRequest(Request request);
-}
--- labs/src/main/java/com/sawin/labs/service/MyModifyService.java
+++
@@ -1,9 +0,0 @@
-package com.sawin.labs.service;
-
-import com.sawin.labs.model.Response;
-
-public interface MyModifyService {
-
-    Response modify(Response response);
-
-}
